87b33064d584f0fa4798cab9d512be05823df56e
Stefan Richter <stefan@02strich.de>
2014-02-06 21:15:02 +0100

    Fix name of Readme file

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer          "Stefan Richter"
 maintainer_email    "stefan@02strich.de"
 license             "MIT"
 description         "Installs/Configures mpd"
-long_description    IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version             "0.0.2"
 
 recipe              "mpd::default"			"Install and configure primary mpd instance"
