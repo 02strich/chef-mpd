957ec18f59eed6542a3af8ad0f1f70d8bd1f28f9
Stefan Richter <stefan@02strich.de>
2014-02-06 21:23:07 +0100

    Adding missing comma

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description         "Installs/Configures mpd"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version             "0.0.2"
 
-recipe              "mpd::default"			"Install and configure primary mpd instance"
+recipe 	            "mpd::default", "Install and configure primary mpd instance"
 
 supports            "debian"
 supports            "ubuntu"
